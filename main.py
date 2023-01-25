7a27e3a2f6158c466456e69c4c55c9d188437882
Claudiu Nemes <langarus@gmail.com>
2023-01-25 22:24:02 +0200

    chore: update folder structure

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from random import randrange
 import time
-from parse_one import bs4_get_search_results
-from write_to_json import write_to_json
+from utils.parse_one import bs4_get_search_results
+from utils.write_to_json import write_to_json
 import csv
 
 keywords = []
+
 with open('keywords.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
