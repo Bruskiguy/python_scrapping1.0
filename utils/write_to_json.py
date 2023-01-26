0a5d0193584e8da769304332fa7d10551087228d
Claudiu Nemes <langarus@gmail.com>
2023-01-26 02:01:43 +0200

    chore: update write to json

--- utils/write_to_json.py
+++ utils/write_to_json.py
@@ -1,9 +1,22 @@
 import json
+import os
+
+file_path = "data.json"
 
 
 def write_to_json(data):
-    with open('data.json', "r") as f:
-        data = json.load(f)
-    data.append(data)
-    with open('data.json', "w") as f:
-        json.dump(data, f)
+    # check if exists
+    if os.path.isfile("data.json"):
+        print("exists")
+        with open('data.json', "r+") as f:
+            prev_data = json.load(f)
+            s = prev_data
+            # data comes as [{"first_item":[...tags+titles]}]
+            # we don't need the wrapping array
+            s.update(data[0])
+            # move cursor to beginning - bc we want to paste the new values on top
+            f.seek(0)
+            json.dump(s, f)
+    else:
+        with open('data.json', "w+") as f:
+            json.dump(data[0], f)
